c7a646d12244e724938ac77fa4489bb089fca0d7
Rodney Urquhart <3329665+RodneyU215@users.noreply.github.com>
2024-06-07 18:55:26 -0700

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,11 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  async rewrites() {
+    return [
+      { source: "/docs", destination: "https://mintlify-c612493e.mintlify.dev/docs" },
+      { source: "/docs/:match*", destination: "https://mintlify-c612493e.mintlify.dev/docs/:match*" }
+    ];
+  }
+};
 
 export default nextConfig;
